a885b2c04d9074a2a0bf9a40188bd2ffd1e4c57d
JerryLiuLYU <liumingtao412@126.com>
2019-01-21 20:09:13 +0800

    写了一行注释

--- WinForm-BlockEncypt/Program.cs
+++ WinForm-BlockEncypt/Program.cs
@@ -16,7 +16,7 @@ namespace WinForm_BlockEncypt
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form1()); // 打开窗体1
         }
     }
 }
